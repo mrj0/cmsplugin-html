58ee62e93d057ea1e3f48fd7c9ca9c927b3012f1
Mike Johnson <mike@publicstatic.net>
2011-04-13 15:27:15 -0700

    fix packaging

--- setup.py
+++ setup.py
@@ -1,4 +1,58 @@
+#!/usr/bin/env python
+
+# setup from https://bitbucket.org/xenofox/cmsplugin-plaintext/
+
 from setuptools import setup, find_packages
+PACKAGE_NAME = 'cmsplugin_html'
+PACKAGE_DIR = PACKAGE_NAME
+
+import os, sys
+
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into compontents (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Tell disutils to put the data_files in platofmr-specific installation
+# locations.
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distuils doesn't have
+# and easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk(PACKAGE_DIR):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append(
+                [dirpath, [os.path.join(dirpath, f) for f in filenames]]
+                )
+# Small hack for working with bdist_wininst
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
+    for file_info in data_files:
+        file_info[0] = '\\PURELIB\\%s' % file_info[0]
+
 setup(
     name='cmsplugin-html',
     version= '0.1',
@@ -10,7 +64,8 @@ setup(
     maintainer_email='mike@publicstatic.net',
     url='https://github.com/mrj0/cmsplugin-html',
     license='MIT',
-    packages=find_packages(),
+    packages=packages,
+    data_files=data_files,
     classifiers=[
         'Development Status :: 1 - Alpha',
         'Environment :: Web Environment',
@@ -25,5 +80,4 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=['setuptools'],
-    requires= ['captcha']
 )
